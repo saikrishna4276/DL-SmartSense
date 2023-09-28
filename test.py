89ab0a85f535077d4444d6ebb3f5503abaee0fff
saikrishna4276 <avulasaikrishna@iitgn.ac.in>
2023-09-28 18:47:50 +0530

    final

---
+++ test.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+import time
+
+from predict import Classify
+
+filename = "application_data\input_image\input_image.jpg"
+        
+classifier = Classify(filename)
+        # print("B")
+result = classifier.recognition()
+
+print(result)
